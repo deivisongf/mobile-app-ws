ae34801f8e8e01fd4cca2143483e2e99b9ae249b
deivison <dguarines@gmail.com>
2019-05-30 22:57:37 -0300

    Create Users Rest Controller class

---
+++ src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.r4s.app.sw.mobileappws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+}
