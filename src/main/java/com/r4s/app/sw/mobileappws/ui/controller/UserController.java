98c5023fe0008ba65d01a99d00045d90d77313f0
deivison <dguarines@gmail.com>
2019-05-30 23:35:23 -0300

    Reading Path Variables with

--- src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package com.r4s.app.sw.mobileappws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") //http:localhost:8080/users
 public class UserController {
 	
-	@GetMapping
-	public String getUser() {
-		return "get user was called";
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "get user was called with userId = " + userId;
 	}
 	
 	@PostMapping
