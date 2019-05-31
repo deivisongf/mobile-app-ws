e53e98ccd4649776841623f44590e1409c6d27c4
Deivison <dguarines@gmail.com>
2019-05-31 12:58:11 -0300

    Returning Object as JSON or XML Representation

--- src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.r4s.app.sw.mobileappws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ public class UserController {
 		return "get user was called with page " + page + " and limit " + limit + " and sort " + sort;
 	}
 	
-	@GetMapping(path="/{userId}")
+	@GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	public UserRest getUser(@PathVariable String userId) {
 		
 		UserRest fakeUser = new UserRest();
