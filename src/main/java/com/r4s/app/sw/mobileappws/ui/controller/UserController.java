dc5e2df48513f61198a0cd56213b4b2353aaea6a
deivison <dguarines@gmail.com>
2019-05-30 23:42:39 -0300

    Reading Query String Request Parameters

--- src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/r4s/app/sw/mobileappws/ui/controller/UserController.java
@@ -6,11 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") //http:localhost:8080/users
 public class UserController {
+	
+	@GetMapping()
+	public String getUser(@RequestParam(value="page") int page, @RequestParam(value="limit") int limit) {
+		return "get user was called with page " + page + " and limit " + limit;
+	}
 	
 	@GetMapping(path="/{userId}")
 	public String getUser(@PathVariable String userId) {
