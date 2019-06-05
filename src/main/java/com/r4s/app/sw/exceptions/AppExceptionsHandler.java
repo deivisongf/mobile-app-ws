d138050dee11d076403aff6c130ca30a17078754
deivison <dguarines@gmail.com>
2019-06-04 22:12:39 -0300

    Catch More Than One Exception with One Method

--- src/main/java/com/r4s/app/sw/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/r4s/app/sw/exceptions/AppExceptionsHandler.java
@@ -27,20 +27,8 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(value = {NullPointerException.class})
-	public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request){
-		
-		String errorMessageDescription = ex.getLocalizedMessage();
-		
-		if(errorMessageDescription == null) errorMessageDescription = ex.toString();
-		
-		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
-		
-		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-	@ExceptionHandler(value = {UserServiceException.class})
-	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
+	@ExceptionHandler(value = {NullPointerException.class, UserServiceException.class})
+	public ResponseEntity<Object> handleSpecificException(NullPointerException ex, WebRequest request){
 		
 		String errorMessageDescription = ex.getLocalizedMessage();
 		
